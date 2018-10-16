57f19e137b32df6c47011a98dc7a18efd06f3009
Zhi Li <zli@babson.edu>
2018-10-16 16:11:25 -0400

    added roster.py

--- misc/roster.py
+++ misc/roster.py
@@ -1,11 +1,4 @@
 import random
 
-class_roster = ['Jonathan Beltran', 'Allison Fernandez', 'Siddhanth Goyal', 'Jingyu He', 'Defne Ikiz', 'Zirui Jiao', 'Pranjal Joshi', 'Dong Hyun Kim', 'Ha Min Ko', 'Kyle Lawson', 'Christine Lee', 'Connie Li',
-                'Qinyi Li', 'Matthew Michalke', 'Ho Wang Alastair Ng', 'Jonghyun Park', 'Alden Pexton', 'Shriya Rathi', 'Waylon Ryan', 'Christian Thompson', 'Angela Tsung', 'Aaron Wendell', 'Sarah Zazyczny', 'Shiyue (Shirley) Zong']
-
-
-# print(random.choice(class_roster))
-print(random.sample(class_roster, 3))
-
-# for name in class_roster:
-#     print(name, len(name))
+class_roster = {'Jonathan Beltran': 0, 'Allison Fernandez': 1, 'Siddhanth Goyal': 0, 'Jingyu He': 0, 'Defne Ikiz': 0, 'Zirui Jiao': 0, 'Pranjal Joshi': 0, 'Dong Hyun Kim': 0, 'Ha Min Ko': 0, 'Kyle Lawson': 0, 'Christine Lee': 1, 'Connie Li': 1,
+                'Qinyi Li': 0, 'Matthew Michalke': 0, 'Ho Wang Alastair Ng': 1, 'Jonghyun Park': 0, 'Alden Pexton': 2, 'Shriya Rathi': 2, 'Waylon Ryan': 1, 'Christian Thompson': 3, 'Angela Tsung': 2, 'Aaron Wendell': 0, 'Sarah Zazyczny': 0, 'Shiyue (Shirley) Zong': 0}
---
+++ misc/test.py
@@ -0,0 +1,10 @@
+def a(b):
+    # if b > 10:
+    #     return True
+    # else:
+    #     return False
+
+    return b > 10
+
+
+print(a(11))
