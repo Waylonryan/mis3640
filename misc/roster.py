6e8e5f93fe5f798b034f9ea6f67637f9993d4d17
Zhi Li <zli@babson.edu>
2018-10-18 12:40:25 -0400

    added session 14

--- misc/roster.py
+++ misc/roster.py
@@ -1,4 +1,44 @@
 import random
 
-class_roster = {'Jonathan Beltran': 0, 'Allison Fernandez': 1, 'Siddhanth Goyal': 0, 'Jingyu He': 0, 'Defne Ikiz': 0, 'Zirui Jiao': 0, 'Pranjal Joshi': 0, 'Dong Hyun Kim': 0, 'Ha Min Ko': 0, 'Kyle Lawson': 0, 'Christine Lee': 1, 'Connie Li': 1,
-                'Qinyi Li': 0, 'Matthew Michalke': 0, 'Ho Wang Alastair Ng': 1, 'Jonghyun Park': 0, 'Alden Pexton': 2, 'Shriya Rathi': 2, 'Waylon Ryan': 1, 'Christian Thompson': 3, 'Angela Tsung': 2, 'Aaron Wendell': 0, 'Sarah Zazyczny': 0, 'Shiyue (Shirley) Zong': 0}
+class_roster = {'Aaron Wendell': 0,
+                'Alden Pexton': 2,
+                'Allison Fernandez': 1,
+                'Angela Tsung': 2,
+                'Christian Thompson': 3,
+                'Christine Lee': 1,
+                'Connie Li': 1,
+                'Defne Ikiz': 0,
+                'Dong Hyun Kim': 0,
+                'Ha Min Ko': 0,
+                'Ho Wang Alastair Ng': 1,
+                'Jingyu He': 0,
+                'Jonathan Beltran': 0,
+                'Jonghyun Park': 0,
+                'Kyle Lawson': 0,
+                'Matthew Michalke': 0,
+                'Pranjal Joshi': 0,
+                'Qinyi Li': 0,
+                'Sarah Zazyczny': 0,
+                'Shiyue (Shirley) Zong': 0,
+                'Shriya Rathi': 2,
+                'Siddhanth Goyal': 0,
+                'Waylon Ryan': 1,
+                'Zirui Jiao': 0}
+
+min_times = min(class_roster.values())
+
+print(min_times)
+
+pool = []
+
+for name in class_roster:
+    if class_roster[name] == min_times:
+        pool.append(name)
+
+
+random_name = random.choice(pool)
+
+print(random_name)
+class_roster[random_name] += 1
+
+# TODO: store the data
