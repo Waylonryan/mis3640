5182a8fbe12a2e0116f61d9e541311537909ed07
Zhi Li <zli@babson.edu>
2018-10-30 17:19:56 -0400

    finished OOP3 on 10/30

--- OOP/OOP3/Point1.py
+++ OOP/OOP3/Point1.py
@@ -7,10 +7,26 @@ class Point:
     attributes: x, y
     """
 
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
 
-def print_point(p):
-    """Print a Point object in human-readable format."""
-    print('({}, {}).'.format(p.x, p.y))
+    def __str__(self):
+        """return a Point object in human-readable format."""
+        return '({}, {}).'.format(self.x, self.y)
+
+    def __add__(self, other):
+        new_point = Point(self.x + other.x, self.y + other.y)
+        return new_point
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+    def __contains__(self, value):
+        return value == self.x or value == self.y
 
 
 def distance_between_points(p1, p2):
@@ -28,18 +44,30 @@ def distance_between_points(p1, p2):
 
 
 def main():
-    my_point = Point()
-    print(Point.__doc__)
-    my_point.x = 3
-    my_point.y = 4
-    print('My point', end=' ')
-    print_point(my_point)
+    defne = Point(4, 3)
+    print(defne)
+    shirley = Point(1, 1)
+    print(defne + shirley)
+    print(defne - shirley)
 
-    print('Is my_point an instance of Point?', isinstance(my_point, Point))
-    print('Is my_point an instance of Rectangle?',
-          isinstance(my_point, Rectangle))
-    print('Does my_point have an attribute x?', hasattr(my_point, 'x'))
-    print('Does my_point have an attribute z?', hasattr(my_point, 'z'))
+    angela = Point(4, 10)
+    print(angela == defne)
+    print(4 in angela)
+    print(5 in angela)
+    # defne.print_point()
+
+    # my_point = Point()
+    # print(Point.__doc__)
+    # my_point.x = 3
+    # my_point.y = 4
+    # print('My point', end=' ')
+    # print_point(my_point)
+
+    # print('Is my_point an instance of Point?', isinstance(my_point, Point))
+    # print('Is my_point an instance of Rectangle?',
+    #       isinstance(my_point, Rectangle))
+    # print('Does my_point have an attribute x?', hasattr(my_point, 'x'))
+    # print('Does my_point have an attribute z?', hasattr(my_point, 'z'))
 
 
 if __name__ == '__main__':
